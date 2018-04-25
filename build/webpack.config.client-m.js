a51efb5b3f76c48885609b1c45a72be737704109
liujianhua <1769908045@qq.com>
2018-04-25 22:50:06 +0800

    ds

--- build/webpack.config.client-m.js
+++ build/webpack.config.client-m.js
@@ -2,6 +2,7 @@ const {resolve} = require('path')
 const merge = require('webpack-merge')
 const base = require('./webpack.config.base-m.js')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const OfflinePlugin = require('offline-plugin')
 module.exports = merge(base, {
   entry: {
     app: resolve(__dirname, '../m/entry-client.js')
@@ -18,6 +19,9 @@ module.exports = merge(base, {
     }
   },
   plugins: [
-    new VueSSRClientPlugin()
+    new VueSSRClientPlugin(),
+    new OfflinePlugin({
+      externals: ['/', '/start', '/document', '/hub', '/about']
+    })
   ]
 })
