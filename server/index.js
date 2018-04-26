e2f2edb18715ec3c0c84442140c8f8765a0574bf
liujianhua <1769908045@qq.com>
2018-04-26 21:23:52 +0800

    ds

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const Koa = require('koa')
-const router = require('./router')
-const app = new Koa()
-
-app.use(router.routes()).use(router.allowedMethods())
-
-app.listen(8888, () => console.log(`DataServer Run In http://localhost:8888`))
